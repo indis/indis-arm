8fdece196ab3c2fefdb27e0b3f25babfe8cacfe4
Vladimir Pouzanov <farcaller@gmail.com>
2012-04-27 13:42:25 +0300

    Updated thumb1 fixture and added special cases that don't follow gcc

--- spec/indis-arm/ual_loader_spec.rb
+++ spec/indis-arm/ual_loader_spec.rb
@@ -2,7 +2,7 @@ require 'indis-arm/ual_loader'
 
 describe Indis::ARM::UalLoader do
   it "should load instructions" do
-    Indis::ARM::UalLoader.instance.matchers.length.should == 31
+    Indis::ARM::UalLoader.instance.matchers.length.should == 33
     Indis::ARM::UalLoader.instance.commons.length.should == 16
   end
   
@@ -13,6 +13,28 @@ describe Indis::ARM::UalLoader do
       opc.gsub!("\t", " ")
       it "parses 0x#{val.strip} as \"#{opc}\"" do
         i = Indis::ARM::UalLoader.instance.map_instruction(val.to_i(16), :thumb)
+        i.should decode_to_opcode(opc)
+      end
+    end
+    
+    special_cases = {
+      0b0000000000100010 => 'movs r2, r4',
+      
+      0b0000100000001010 => 'lsrs r2, r1, #0',
+      0b0000100001001010 => 'lsrs r2, r1, #1',
+      
+      0b0001000000000001 => 'asrs r1, r0, #0',
+      
+      0b0100001001100110 => 'rsbs r6, r4, #0',
+      0b0100001001111001 => 'rsbs r1, r7, #0',
+      
+      0x4373 => 'muls r3, r6, r3',
+      0x4342 => 'muls r2, r0, r2',
+    }
+    
+    special_cases.each do |val, opc|
+      it "parses 0x#{val.to_s 16} as \"#{opc}\"" do
+        i = Indis::ARM::UalLoader.instance.map_instruction(val, :thumb)
         i.should decode_to_opcode(opc)
       end
     end
